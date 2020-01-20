a7c7b46d36912056f62d61636316ac3f0fc1d673
Eric Tu <tu.eric@hotmail.com>
2020-01-20 15:42:00 -0500

    ticket cbor tuple

--- blockchain/blocks/src/ticket.rs
+++ blockchain/blocks/src/ticket.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crypto::VRFResult;
+use encoding::{de::{self, Deserializer} , ser::{self, Serializer}, Cbor};
 use serde::{Deserialize, Serialize};
 
 /// A Ticket is a marker of a tick of the blockchain's clock.  It is the source
@@ -24,7 +25,7 @@ impl Ticket {
 }
 
 /// PoSt election candidates
-#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Default)]
 pub struct EPostTicket {
     partial: Vec<u8>,
     sector_id: u64,
@@ -32,9 +33,71 @@ pub struct EPostTicket {
 }
 
 /// Proof of Spacetime election proof
-#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Default)]
 pub struct EPostProof {
     proof: Vec<u8>,
     post_rand: Vec<u8>,
     candidates: Vec<EPostTicket>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CborEPostTicket (
+    Vec<u8>,    // partial
+    u64,        // sector_id
+    u64         // challenge_index
+);
+
+#[derive(Serialize, Deserialize)]
+pub struct CborEPostProof (
+    Vec<u8>,             // proof
+    Vec<u8>,             // post_rand
+    Vec<EPostTicket>     // candidates
+);
+
+impl ser::Serialize for EPostTicket{
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error> where
+        S: Serializer {
+        let value: CborEPostTicket = CborEPostTicket(
+            self.partial.clone(),
+            self.sector_id,
+            self.challenge_index
+        );
+        CborEPostTicket::serialize(&value, serializer)
+    }
+}
+
+impl<'de> de::Deserialize<'de> for EPostTicket {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error> where
+        D: Deserializer<'de> {
+        let cm = CborEPostTicket::deserialize(deserializer)?;
+        Ok(Self{
+            partial: cm.0,
+            sector_id: cm.1,
+            challenge_index: cm.2
+        })
+    }
+}
+
+impl ser::Serialize for EPostProof {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error> where
+        S: Serializer {
+        let value: CborEPostProof = CborEPostProof(
+            self.proof.clone(),
+            self.post_rand.clone(),
+            self.candidates.clone()
+        );
+        CborEPostProof::serialize(&value, serializer)
+    }
+}
+
+impl<'de> de::Deserialize<'de> for EPostProof {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error> where
+        D: Deserializer<'de> {
+        let cm = CborEPostProof::deserialize(deserializer)?;
+        Ok(Self{
+            proof: cm.0,
+            post_rand: cm.1,
+            candidates: cm.2
+        })
+    }
 }
